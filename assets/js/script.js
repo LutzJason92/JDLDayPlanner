a44bdb771ada7e337b2dcc7724f72c61df538196
Jason <LutzJason92@gmail.com>
2021-11-14 14:25:09 -0700

    color change for 8am row only
    wrote frunction to change color of row but it is displaying future only. console logged variables should make logical sense per if else statements but the incorrect color is being displayed. *flips desk over*

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,46 +8,47 @@
 var weekDayEl = moment().format("dddd");
 var currentDayEl = moment().format("MMM Do, YYYY"); 
 $("#currentDay").text("Today is " + weekDayEl + " " + currentDayEl+ ".");
-var currentTimeEl = moment().format("h:mm A");
+var currentTimeEl = moment().format("hh:mm A");
 $("#currentTime").text("The time is " + currentTimeEl);
 
-// 
-var setTimeEL = moment("8:00 AM", ["h:mm A"]).format("h:mm A");
-$("#eightAm").text(setTimeEL);
-
-var setTimeEL = moment("9:00 AM", ["h:mm A"]).format("h:mm A");
-$("#nineAm").text(setTimeEL);
-
-var setTimeEL = moment("10:00 AM", ["h:mm A"]).format("h:mm A");
-$("#tenAm").text(setTimeEL);
-
-var setTimeEL = moment("11:00 AM", ["h:mm A"]).format("h:mm A");
-$("#elevenAm").text(setTimeEL);
-
-var setTimeEL = moment("12:00 PM", ["h:mm A"]).format("h:mm A");
-$("#twelvePm").text(setTimeEL);
-
-var setTimeEL = moment("1:00 PM", ["h:mm A"]).format("h:mm A");
-$("#onePm").text(setTimeEL);
-
-var setTimeEL = moment("2:00 PM", ["h:mm A"]).format("h:mm A");
-$("#twoPm").text(setTimeEL);
-
-var setTimeEL = moment("3:00 PM", ["h:mm A"]).format("h:mm A");
-$("#threePm").text(setTimeEL);
-
-var setTimeEL = moment("4:00 PM", ["h:mm A"]).format("h:mm A");
-$("#fourPm").text(setTimeEL);
-
-var setTimeEL = moment("5:00 PM", ["h:mm A"]).format("h:mm A");
-$("#fivePm").text(setTimeEL);
-
-var setTimeEL = moment("6:00 PM", ["h:mm A"]).format("h:mm A");
-$("#sixPm").text(setTimeEL);
+var time = moment();
 
 
+var setTimeEight = moment("8:00 AM", ["hh:mm A"]).format("hh:mm A");
+$("#eightAm").text(setTimeEight);
 
-// buttons and text entry
+var setTimeNine = moment("9:00 AM", ["hh:mm A"]).format("hh:mm A");
+$("#nineAm").text(setTimeNine);
+
+var setTimeTen = moment("10:00 AM", ["hh:mm A"]).format("hh:mm A");
+$("#tenAm").text(setTimeTen);
+
+var setTimeEleven = moment("11:00 AM", ["hh:mm A"]).format("hh:mm A");
+$("#elevenAm").text(setTimeEleven);
+
+var setTimeTwelve = moment("12:00 PM", ["hh:mm A"]).format("hh:mm A");
+$("#twelvePm").text(setTimeTwelve);
+
+var setTimeOne = moment("1:00 PM", ["hh:mm A"]).format("hh:mm A");
+$("#onePm").text(setTimeOne);
+
+var setTimeTwo = moment("2:00 PM", ["hh:mm A"]).format("hh:mm A");
+$("#twoPm").text(setTimeTwo);
+
+var setTimeThree = moment("3:00 PM", ["hh:mm A"]).format("hh:mm A");
+$("#threePm").text(setTimeThree);
+
+var setTimeFour = moment("4:00 PM", ["hh:mm A"]).format("hh:mm A");
+$("#fourPm").text(setTimeFour);
+
+var setTimeFive = moment("5:00 PM", ["hh:mm A"]).format("hh:mm A");
+$("#fivePm").text(setTimeFive);
+
+var setTimeSix = moment("6:00 PM", ["hh:mm A"]).format("hh:mm A");
+$("#sixPm").text(setTimeSix);
+
+
+// ------------------buttons and text entry---------------------
 $("#eightBtn").on("click", function() {
     var textEntry = $("#eightText").val();
     localStorage.setItem("8:00AM", textEntry)
@@ -114,31 +115,26 @@ $("#sixBtn").on("click", function() {
 })
 $("#sixText").val(localStorage.getItem("6:00PM"));
 
- //if statements for color coded for past, present or future
+ // --------------if statements for color coded for past, present or future---------------
 
-// if moment current is <= mometn set past          light grey
-// if moment current is === mometn set present      light blue
-// if moment current is >= mometn set future        light green
 
-var timeOfDay = [ "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM" ]
-
-var timeColor = document.querySelectorAll(".form-control")
-
-function textBoxColor() {
-    if (timeOfDay < moment("8:00 AM", ["h:mm A"]).format("h:mm A") ) {
+var currentTime = moment().format("hh:mm A")
+var timeColor = document.querySelector(".form-control.eightColor")
+function eightBoxColor() {
+    if (currentTime > setTimeEight) {
         $(timeColor).addClass("past");
     }
-    else if (timeOfDay === moment("8:00 AM", ["h:mm A"]).format("h:mm A")) {
+    else if (currentTime === setTimeEight) {
         $(timeColor).addClass("present");
     }
-    else (timeOfDay > moment("8:00 AM", ["h:mm A"]).format("h:mm A"));{
+    else (currentTime < setTimeEight);{
         $(timeColor).addClass("future");
     }
-}
+}; 
+eightBoxColor()
 
-
-textBoxColor()
-console.log(timeOfDay)
-console.log(timeColor)
 console.log(currentTimeEl)
+console.log(timeColor)
+console.log(setTimeEight)
+
 
